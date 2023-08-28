f1ad16d6f163da868fd0ca98358b4e116ae88604
Jinesh Salot <jinesh.salot@netmeds.com>
2023-08-29 00:18:09 +0530

    "login page modify"

--- clientapp/src/components/login/index.jsx
+++ clientapp/src/components/login/index.jsx
@@ -6,12 +6,27 @@ function Login({setUser,setSecret}) {
   const [username,setUsername]=useState("");
   const [password,setPassword] = useState("");
   const [triggerLogin,resultLogin] = usePostLoginMutation();
-  const [triggerSignUp] = usePostSignUpMutation();
+  const [triggerSignUp,resultSignUp] = usePostSignUpMutation();
+  const [isAuthenticated,setIsAuthenticated]= useState(false);
+  const [missingValue,setMissingValue]=useState(false);
+
   const handleRegister =()=>{
-    triggerSignUp({username,password})
+    if(username && password){
+      setMissingValue(false)
+      triggerSignUp({username,password})
+    }
+    else{
+      setMissingValue(true)
+    }
   }
   const handleLogin =()=>{
-    triggerLogin({username,password})
+    if(username && password){
+      setMissingValue(false)
+      triggerLogin({username,password})
+    }
+    else{
+      setMissingValue(true)
+    }
   }
   useEffect(()=>{
     if(resultLogin.data?.response)
@@ -20,11 +35,29 @@ function Login({setUser,setSecret}) {
         setSecret(password);
     }
   },[resultLogin.data])
+  useEffect(()=>{
+    if(resultSignUp?.data?.response?.is_authenticated){
+      setIsRegister(false);
+      setIsAuthenticated(true);
+    }
+  },[resultSignUp.data])
+  useEffect(()=>{
+    if(isAuthenticated){
+      setTimeout(()=>{
+        setIsAuthenticated(false)
+      },[2000])
+    }
+  },[isAuthenticated])
   return (
     <div className="login-page">
-        <h2 className="title">ChatApp With Chatgpt</h2>
+        <h2 className="login-title">ChatApp With Chatgpt</h2>
+        {isAuthenticated ?(
+        <p className="login-title">
+             User Register succesfully!!
+        </p>):(<></>)
+        }
         <p className="register-change" onClick={()=>{setIsRegister(!isRegister)}}>
-            {isRegister ? "Alreay a user?" : "Are you a new user"}
+            {isRegister ? "Alreay a user? click here" : "Are you a new user? click here"}
         </p>
         <div>
             <input className="login-input" type='text' placeholder='Username' value={username}
@@ -32,15 +65,21 @@ function Login({setUser,setSecret}) {
             <input className="login-input" type='password' placeholder='Password' value={password}
                 onChange={(e)=>{setPassword(e.target.value)}}/>
         </div>
+        
+        {missingValue ?
+        <p className='login-title'>
+           ** Please enter username and password properly **
+        </p>:<></>}
+        
         <div className="login-actions">
             {isRegister ?(
-                <button type='button' onClick={handleRegister}>
+                <div className='btn'  onClick={handleRegister}>
                     Register
-                </button>
+                </div>
             ):(
-                <button type='button' onClick={handleLogin}>
+                <div className='btn'  onClick={handleLogin}>
                     Login
-                </button>
+                </div>
             )}
         </div>
     </div>
